48d0aa55e85a882c22d838bcb96bdd01f2cad97f
DavidToribio1 <dtmuzik1@gmail.com>
2020-12-01 20:13:19 -0500

    playlist done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,35 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int up = 0;
+        int down =0;
+        //down playlist
+        for(int i =startIndex; i < playList.length; i++){
+            if (playList[i].equals(selection)){
+                break;
+
+            }
+            if (i + 1 > playList.length -1){
+                i= -1;
+            }
+            down += 1;
+        }
+        //up playlist
+        for (int i = startIndex; i >= 0; i--){
+            if (playList[i].equals(selection)){
+                break;
+            }
+            if (i - 1 < 0){
+                i=playList.length;
+            }
+            up += 1;
+        }
+        if (down > up){
+            return up;
+        }
+        else {
+            return down;
+        }
+        }
     }
-}
+
